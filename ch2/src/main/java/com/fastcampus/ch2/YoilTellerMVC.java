95434396274a69b297dd27e15cbb5d4d8238ba6b
oni <rainbow2774@naver.com>
2022-03-22 15:04:44 +0900

    2-12 claer

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,53 +1,46 @@
 package com.fastcampus.ch2;
 
+import java.util.Calendar;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Calendar;
-
-@Controller //프로그램 등록!
+@Controller
 public class YoilTellerMVC {
-	@RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC?year=2021&month=10&day=1 , URL 연결
-//		public void main(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		public ModelAndView main(int year, int month, int day) throws IOException {
-			
-			ModelAndView mv = new ModelAndView();
-			//1.유효성 검사
-//			if(!isValid(year, month, day))
-//				return "yoilError";
-		
-			//2.요일 계산
-			char yoil = getYoil(year, month, day);
-			
-			//3.계산한 결과를 model에 저장 
-			mv.addObject("year", year);
-			mv.addObject("month", month);
-			mv.addObject("day", day);
-			mv.addObject("yoil", yoil);
-			
-			//4.결과를 보여줄 view를 지정
-			mv.setViewName("yoil");
-			
-			return mv;
-//		return "yoil"; // /WEB-INF/view/yoil.jsp 에있는걸로 보여줘
-	}
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+    public String main(int year, int month, int day, Model model) {
+ 
+        // 1. 유효성 검사
+    	if(!isValid(year, month, day)) 
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
+    	
+        // 2. 처리
+    	char yoil = getYoil(year, month, day);
 
-	private boolean isValid(int year, int month, int day) {
-		
-		return true;
-	}
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
+        
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
+    }
+    
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day);
 
-	private char getYoil(int year, int month, int day) {
-		Calendar cal = Calendar.getInstance();
-		cal.set(year, month - 1, day);
-
-		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-		return " 일월화수목금토".charAt(dayOfWeek);
-	}
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(int year, int month, int day) {    
+    	if(year==-1 || month==-1 || day==-1) 
+    		return false;
+    	
+    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
+    }
 }
