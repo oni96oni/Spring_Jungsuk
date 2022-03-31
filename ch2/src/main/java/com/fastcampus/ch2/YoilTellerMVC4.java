e2bbc7f605ce9cabcc490916fb5faaa6f48ebacc
oni <rainbow2774@naver.com>
2022-03-31 22:58:47 +0900

    2-17 clear...

--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC4.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC4.java
@@ -9,34 +9,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class YoilTellerMVC {
+public class YoilTellerMVC4 {
 	@ExceptionHandler(Exception.class)
 	public String catcher(Exception ex) {
 		ex.printStackTrace();
 		return "yoilError";
 	}
 	
-    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
-    public String main(@RequestParam(required=true)int year, @RequestParam(required=true)int month, @RequestParam(required=true)int day, @RequestParam(required=true)Model model) {
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC4
+    public String main(MyDate date, Model model) {
  
         // 1. 유효성 검사
-    	if(!isValid(year, month, day)) 
+    	if(!isValid(date)) 
     		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
     	
         // 2. 처리
-    	char yoil = getYoil(year, month, day);
+    	char yoil = getYoil(date);
 
         // 3. Model에 작업 결과 저장
-        model.addAttribute("year", year);
-        model.addAttribute("month", month);
-        model.addAttribute("day", day);
+        model.addAttribute("Mydate", date);
         model.addAttribute("yoil", yoil);
         
         // 4. 작업 결과를 보여줄 View의 이름을 반환
         return "yoil"; // /WEB-INF/views/yoil.jsp
     }
     
-    private char getYoil(int year, int month, int day) {
+    private boolean isValid(MyDate date) {
+    	return isValid(date.getYear(), date.getMonth(), date.getDay());
+	}
+
+	private char getYoil(MyDate date) {
+		return getYoil(date.getYear(), date.getMonth(), date.getDay());
+	}
+
+	private char getYoil(int year, int month, int day) {
         Calendar cal = Calendar.getInstance();
         cal.set(year, month - 1, day);
 
