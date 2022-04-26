96c46f32251f0663741340eb76b7d5c65c2ad669
oni <rainbow2774@naver.com>
2022-04-26 15:13:49 +0900

    redirect와 forward

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -9,31 +9,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegisterController {
-	
-//	@RequestMapping(value="/register/save", method={RequestMethod.GET,RequestMehod.POST})
-//	@RequestMapping("/register/add") 신규회원 가입 화면
-//	@GetMapping("/register/add")
-//	public String register() {
-//		return "registerForm"; // WEB-INF/view/registerForm.jsp
-//	}
-	
-//	@RequestMapping(value="/register/save", method=RequestMethod.POST) 길어서 아래처럼 쓴다
+
+	//	@RequestMapping(value="/register/save", method={RequestMethod.GET,RequestMehod.POST})
+	//	@RequestMapping("/register/add") 신규회원 가입 화면
+	//	@GetMapping("/register/add")
+	//	public String register() {
+	//		return "registerForm"; // WEB-INF/view/registerForm.jsp
+	//	}
+
+	//	@RequestMapping(value="/register/save", method=RequestMethod.POST) 길어서 아래처럼 쓴다
 	@PostMapping("/register/save") //spreing4.3부터 포스트매핑추가
 	public String save(User user, Model m) throws Exception{
 		//1.유효성 검사
 		if(!isValid(user)) {
 			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
-			
+
 			m.addAllAttributes("msg",msg);
 			return "redirect:/register/add?msg="+msg; //URL 재작성(rewriting)
 		}
-		
+
 		//2. DB에 신규회원 정보를 저장
 		return "registerInfo";
 	}
 
 	private boolean isValid(User user) {
-	
-	return false;
-}
+		return false;
+	}
 }
