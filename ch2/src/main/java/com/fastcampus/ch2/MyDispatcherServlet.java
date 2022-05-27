e7d39a290582da15db4d86a99d2159ec13274b83
oni <rainbow2774@naver.com>
2022-05-27 13:46:16 +0900

    스프링이 어떻게 메서드의 매개변수에 맞게 값들을 넣어주고 변환해주는가?

---
+++ ch2/src/main/java/com/fastcampus/ch2/MyDispatcherServlet.java
@@ -0,0 +1,118 @@
+package com.fastcampus.ch2;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
+
+// @Controller + @RequestMapping을 합친것이 @WebServlet 서블릿에서는 메서드단위로 맵핑이 안되고 클래스단위로만 맵핑이 된다! 그래서 서블릿은 스프링에비해서 크래스를 많이 만들어야하는단점 보유
+@WebServlet("/myDispatcherServlet")  // http://localhost/ch2/myDispatcherServlet?year=2021&month=10&day=1
+public class MyDispatcherServlet extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Map    map = request.getParameterMap(); //getParameterMap을 썻으니 year, month, day를 map의 형식으로 가져온다
+		Model  model = null;
+		String viewName = "";
+		
+		try {
+			Class clazz = Class.forName("com.fastcampus.ch2.YoilTellerMVC");
+			Object obj = clazz.newInstance();
+			
+      			// 1. main메서드의 정보를 얻는다.
+			Method main = clazz.getDeclaredMethod("main", int.class, int.class, int.class, Model.class);
+			
+      			// 2. main메서드의 매개변수 목록(paramArr)을 읽어서 메서드 호출에 사용할 인자 목록(argArr)을 만든다. 동적으로!
+			Parameter[] paramArr = main.getParameters();
+			Object[] argArr = new Object[main.getParameterCount()];
+
+			for(int i=0;i<paramArr.length;i++) {
+				String paramName = paramArr[i].getName();
+				Class  paramType = paramArr[i].getType();
+				Object value = map.get(paramName);
+
+				// paramType중에 Model이 있으면, 생성 & 저장 
+				if(paramType==Model.class) {
+					argArr[i] = model = new BindingAwareModelMap();
+				} else if(paramType==HttpServletRequest.class) {
+					argArr[i] = request;
+				} else if(paramType==HttpServletResponse.class) {
+					argArr[i] = response;					
+				} else if(value != null) {  // map에 paramName이 있으면,
+					// value와 parameter의 타입을 비교해서, 다르면 변환해서 저장 
+					String strValue = ((String[])value)[0];	// getParameterMap()에서 꺼낸 value는 String배열이므로 변환 필요, 그냥 넣으면 오류발생 Key는 String인데 Value는 String[]이라서
+					argArr[i] = convertTo(strValue, paramType);				
+				} 
+			}
+			
+			// 3. Controller의 main()을 호출 - YoilTellerMVC.main(int year, int month, int day, Model model)
+			viewName = (String)main.invoke(obj, argArr); 	
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+				
+		// 4. 텍스트 파일을 이용한 rendering
+		render(model, viewName, response);			
+	} // main
+	
+	private Object convertTo(Object value, Class type) {
+		if(type==null || value==null || type.isInstance(value)) // 타입이 같으면 그대로 반환 
+			return value;
+		
+		// 타입이 다르면, 변환해서 반환
+		if(String.class.isInstance(value) && type==int.class) { // String -> int
+			return Integer.valueOf((String)value);
+		} else if(String.class.isInstance(value) && type==double.class) { // String -> double
+			return Double.valueOf((String)value);
+		}
+			
+		return value;
+	}
+		
+	private String getResolvedViewName(String viewName) {
+		return getServletContext().getRealPath("/WEB-INF/views") +"/"+viewName+".jsp";
+	}
+	
+	private void render(Model model, String viewName, HttpServletResponse response) throws IOException {
+		String result = "";
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("utf-8");
+		PrintWriter out = response.getWriter(); //이렇게하면 콘솔이 아니라 브라우져로 출력
+		
+		// 1. 뷰의 내용을 한줄씩 읽어서 하나의 문자열로 만든다.
+		Scanner sc = new Scanner(new File(getResolvedViewName(viewName)), "utf-8");
+		
+		while(sc.hasNextLine())
+			result += sc.nextLine()+ System.lineSeparator();
+		
+		// 2. model을 map으로 변환 
+		Map map = model.asMap();
+		
+		// 3.key를 하나씩 읽어서 template의 ${key}를 value바꾼다.
+		Iterator it = map.keySet().iterator();
+		
+		while(it.hasNext()) {
+			String key = (String)it.next();
+
+			// 4. replace()로 key를 value 치환한다.
+			result = result.replace("${"+key+"}", map.get(key)+"");
+		}
+		
+		// 5.렌더링 결과를 출력한다.
+		out.println(result);
+	}
+}
--- ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC.java
@@ -1,56 +1,46 @@
 package com.fastcampus.ch2;
 
+import java.util.Calendar;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Calendar;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class YoilTellerMVC {
-		@RequestMapping("/getYoilMVC")
-		public ModelAndView main(int year, int month, int day) throws IOException {
-			//이 String이 view의 이름이다.
-			
-			ModelAndView mv = new ModelAndView();
-			//1.유효성 검사
-			
-//			if(!isValid(year,month,day)) {
-//				return "yoilError";
-//			}
-			
-//			// 2. 요일 계산
-			char yoil = getYoil(year, month, day);
-			
-			// 3. 계산한 결과를 model에 저장
-			mv.addObject("year", year);
-			mv.addObject("month", month);
-			mv.addObject("day", day);
-			mv.addObject("yoil", yoil);
-			
-			// 4. 결과를 보여줄 view를 지정
-			mv.setViewName("yoil");
-			
-			return mv;
-			
-//			return "yoil"; // /WEB-INF/views/yoil.jsp 위치는 항상 동일하니까 yoil만 써도 알아먹는다. servlet-context에서 접두 점미를 붙여주기때문
+    @RequestMapping("/getYoilMVC") // http://localhost/ch2/getYoilMVC
+    public String main(int year, int month, int day, Model model) {
+ 
+        // 1. 유효성 검사
+    	if(!isValid(year, month, day)) 
+    		return "yoilError";  // 유효하지 않으면, /WEB-INF/views/yoilError.jsp로 이동
+    	
+        // 2. 처리
+    	char yoil = getYoil(year, month, day);
 
-	}
+        // 3. Model에 작업 결과 저장
+        model.addAttribute("year", year);
+        model.addAttribute("month", month);
+        model.addAttribute("day", day);
+        model.addAttribute("yoil", yoil);
+        
+        // 4. 작업 결과를 보여줄 View의 이름을 반환
+        return "yoil"; // /WEB-INF/views/yoil.jsp
+    }
+    
+    private char getYoil(int year, int month, int day) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, day);
 
-		private boolean isValid(int year, int month, int day) {
-			return true;
-		}
-
-		private char getYoil(int year, int month, int day) {
-			Calendar cal = Calendar.getInstance();
-			cal.set(year, month - 1, day);
-
-			int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK); //1:일요일, 2:월요일, ...
-			return " 일월화수목금토".charAt(dayOfWeek);
-		}
+        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+        return " 일월화수목금토".charAt(dayOfWeek);
+    }
+    
+    private boolean isValid(int year, int month, int day) {    
+    	if(year==-1 || month==-1 || day==-1) 
+    		return false;
+    	
+    	return (1<=month && month<=12) && (1<=day && day<=31); // 간단히 체크 
+    }
 }
