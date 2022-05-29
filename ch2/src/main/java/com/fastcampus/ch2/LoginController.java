fadb5f3c6419d3c76f20c7b4354cf0cd5ac8333e
oni <rainbow2774@naver.com>
2022-05-29 21:04:35 +0900

    @GetMapping,@PostMapping,redirect와 forward 흐름 이해, 쿠키 생성/제거 과정

---
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -0,0 +1,52 @@
+package com.fastcampus.ch2;
+
+import java.net.URLEncoder;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	@GetMapping("/login")
+	public String loginForm() {
+		return "loginForm";
+	}
+	
+	@PostMapping("/login")
+	public String login(String id, String pwd, boolean rememberID, HttpServletResponse response) throws Exception {
+		System.out.println("id="+id);
+		System.out.println("pwd="+pwd);
+		System.out.println("rememberID="+rememberID);
+		// 1. id와 pwd를 확인
+		if(!loginCheck(id,pwd)) {
+			// 2-1. 일치하지 않으면, loginForm으로 이동
+			String msg = URLEncoder.encode("아이디 또는 패스워드가 일치하지 않습니다.", "utf-8");
+			
+			return "redirect:/login/login?msg="+msg;
+		}
+		// 2-2. id와 pwd가 일치하면, 홈으로 이동 일치하지않으면, loginForm으로 이동
+		if(rememberID) {
+			//	1. 쿠키를 생성
+			Cookie cookie = new Cookie("id", id);
+			// 	2. 응답에 저장
+			response.addCookie(cookie);
+		} else {
+			//쿠키를 삭제
+			Cookie cookie = new Cookie("id", id);
+			cookie.setMaxAge(0); 
+			response.addCookie(cookie);
+		}
+		// 	3. 홈으로 이동
+		return "redirect:/";
+	}
+
+	private boolean loginCheck(String id, String pwd) {
+		return "asdf".equals(id) && "1234".equals(pwd);
+	}
+}
--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -12,12 +12,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
-//	@RequestMapping(value = "/register/save", method={RequestMethod.GET, RequestMethod.POST}) GET,POST요청 둘다 허용한다는 의미
-//	@RequestMapping("/register/add") 신규회원 가입 화면
-//	@GetMapping("/register/add")
-//	public String register() {
-//		return "registerForm"; //WEB-INF/view/registerForm.jsp
-//	}
+	@RequestMapping(value = "/register/add", method={RequestMethod.GET, RequestMethod.POST}) //GET,POST요청 둘다 허용한다는 의미
+	public String register() {
+		return "registerForm"; //WEB-INF/view/registerForm.jsp
+	}
 	
 //	@RequestMapping(value = "/register/save", method=RequestMethod.POST) 이렇게 써도되지만 너무길어 그래서 새로운 애너테이션 @PostMapping 사용 할거야 그런데 @PostMapping은 4.3부터 나왔어
 	@PostMapping("/register/save")
@@ -27,7 +25,7 @@ public class RegisterController {
 			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
 			
 			m.addAttribute("msg", msg);
-			return "redirect:/register/add";
+			return "forward:/register/add";
 //			return "redirect:/register/add?msg="+msg; //URL재작성(rewriting) 위에 방법이 모델을 통해서 전달하는 방법.
 		}
 		
@@ -36,6 +34,6 @@ public class RegisterController {
 	}
 
 	private boolean isValid(User user) {
-		return true;
+		return false;
 	}
 }
