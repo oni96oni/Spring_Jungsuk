54aaf2f26666a9e84182dd5344ebb9c998b5bfc0
oni <rainbow2774@naver.com>
2022-06-02 02:44:32 +0900

    데이터의 변환과 검증 Validation의 과정 이해

--- ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
+++ ch2/src/main/java/com/fastcampus/ch2/RegisterController.java
@@ -1,12 +1,15 @@
 package com.fastcampus.ch2;
 
-import java.net.URLEncoder;
+import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,10 +23,14 @@ public class RegisterController {
 	@InitBinder
 	public void toDate(WebDataBinder binder) {
 		ConversionService conversionService = binder.getConversionService();
-		System.out.println("conversionService = " + conversionService);
+//		System.out.println("conversionService = " + conversionService);
 //		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 //		binder.registerCustomEditor(Date.class, new CustomDateEditor(df,false));
 		binder.registerCustomEditor(String[].class, new StringArrayPropertyEditor("#"));
+//		binder.setValidator(new UserValidator()); // UserValidator를 WebDataBinder의 로컬 validator로 등록
+//		binder.addValidators(new UserValidator());
+		List<Validator> validatorList = binder.getValidators();
+		System.out.println("validatorList = " + validatorList);
 	}
 	
 	@RequestMapping(value = "/add", method={RequestMethod.GET, RequestMethod.POST}) //GET,POST요청 둘다 허용한다는 의미
@@ -33,17 +40,27 @@ public class RegisterController {
 	
 //	@RequestMapping(value = "/register/save", method=RequestMethod.POST) 이렇게 써도되지만 너무길어 그래서 새로운 애너테이션 @PostMapping 사용 할거야 그런데 @PostMapping은 4.3부터 나왔어
 	@PostMapping("/save")
-	public String save(User user, BindingResult result, Model m) throws Exception {
+	public String save(@Valid User user, BindingResult result, Model m) throws Exception {
 		System.out.println("result="+result);
 		System.out.println("user="+user);
-		// 1. 유효성 검사
-		if(!isValid(user)) {
-			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
-			
-			m.addAttribute("msg", msg);
-			return "forward:/register/add";
-//			return "redirect:/register/add?msg="+msg; //URL재작성(rewriting) 위에 방법이 모델을 통해서 전달하는 방법.
+		
+		// 수동 검증 - Validator를 직접 생성하고, validate()를 직접 호출
+//		UserValidator userValidator = new UserValidator();
+//		userValidator.validate(user, result);
+		
+		//User객체를 검증한 결과 에러가 있으면, registerForm을 이용해서 에러를 보여줘야한다.
+		if(result.hasErrors()) {
+			return "registerForm";
 		}
+		
+//		// 1. 유효성 검사
+//		if(!isValid(user)) {
+//			String msg = URLEncoder.encode("id를 잘못 입력하셨습니다.", "utf-8");
+//			
+//			m.addAttribute("msg", msg);
+//			return "forward:/register/add";
+////			return "redirect:/register/add?msg="+msg; //URL재작성(rewriting) 위에 방법이 모델을 통해서 전달하는 방법.
+//		}
 		
 		// 2. DB에 신규회원 정보를 저장
 		return "registerInfo";
